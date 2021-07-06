96376f5cb4c4eba497914183ec289bb8ca31ce9b
Enes Varcan <enesvarcan@gmail.com>
2021-07-06 09:28:18 +0300

    Configure app start

--- src/notifier/index.js
+++ src/notifier/index.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const CronJob = require('cron').CronJob
+const morgan = require('morgan')
+const controller = require('./controllers/controller')
+
+require('dotenv').config()
 
 var app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(require('./router'))
+app.use(morgan('combined'))
 
-app.listen("8080", () => {
-    console.log("Listening 8080...")
+/* Cron patterns
+    '0 18 * * *' : Fire at 18:00 every day
+    '* * * * * *' : Fire at every second
+    '* * * * *' : Fire at every minute
+*/
+
+var job = new CronJob('* * * * *', controller.notifyUsers, null, true)
+job.start()
+
+app.listen(process.env.PORT, () => {
+    console.log("*-*-*-*-*-*- Todoapp Notifier Service started -*-*-*-*-*-*")
 })
